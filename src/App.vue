0b15eb78ecb4fe7d717bd4d2ccc47d1dc47581bb
shaghayeghmalekpour <msh.shaqa@gmail.com>
2021-12-14 10:56:25 +0330

    made todolist with vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/layout/AddTodo";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return{
+      todos: [
+        // {
+        //   id: 1,
+        //   title: "todo one",
+        //   completed: false
+        // },
+        // {
+        //   id: 2,
+        //   title: "todo two",
+        //   completed: true
+        // },
+        // {
+        //   id: 3,
+        //   title: "todo three",
+        //   completed: false
+        // }
+      ]
+    }
+  },
+  methods:{
+    // deleteTodo(id){
+    //   axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    //   .then(res =>this.todos = this.todos.filter(todo => todo.id !== id))
+    //       console.log(res)
+    //   .catch(err => console.log(err))
+    //
+    // },
+    addTodo(newTodo){
+      const { title,completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos',{
+        title,
+        completed
+      })
+          .then( res => this.todos =[...this.todos, res.data ])
+          .catch(err => console.log(err))
+
+    }
+  },
+  created() {
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then( res => this.todos = res.data )
+    .catch(err => console.log(err))
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.4;
+}
+.btn{
+  display: inline-block;
+  border: none;
+  color: white;
+  background: #555;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover{
+  background: #666;
 }
 </style>
